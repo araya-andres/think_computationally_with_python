b25d5b31b69f35d0ed79c1a4d630f851d52dd762
Andres Araya <araya.andres@gmail.com>
2023-08-20 15:30:24 -0600

    use enum for direction/move

--- solving_a_maze/solve_maze.py
+++ solving_a_maze/solve_maze.py
@@ -1,65 +1,78 @@
 from __future__ import annotations
 
 import sys
-from typing import List, Tuple
+from typing import List, Optional, Tuple
+from enum import IntEnum, StrEnum
 
-NORTH = 0
-EAST = 1
-SOUTH = 2
-WEST = 3
 
-WALL = "*"
-STARTING_POINT = "o"
-EXIT_POINT = "x"
-VISITED = "v"
+class Direction(IntEnum):
+    NORTH = 0
+    EAST = 1
+    SOUTH = 2
+    WEST = 3
 
-RIGHT = 1
-FORWARD = 0
-LEFT = 3
-BACKWARD = 2
+
+class Move(IntEnum):
+    FORWARD = 0
+    RIGHT = 1
+    BACKWARD = 2
+    LEFT = 3
+
+
+class Cell(StrEnum):
+    EMPTY = " "
+    EXIT_POINT = "x"
+    STARTING_POINT = "o"
+    VISITED = "v"
+    WALL = "*"
+
+
+STEP = ((0, -1), (1, 0), (0, 1), (-1, 0))
 
 
 def load_from_file(filename: str) -> List[List[str]]:
     maze = []
     with open(filename, "r", encoding="utf-8") as reader:
         while line := reader.readline():
-            row = [cell_value for cell_value in line.strip()]
-            for pos_x, cell_value in enumerate(row):
-                pos_y = len(maze)
-                if cell_value == STARTING_POINT:
-                    starting_point = (pos_x, pos_y)
-                elif cell_value == EXIT_POINT:
-                    end_point = (pos_x, pos_y)
+            row = [cell for cell in line.strip()]
+            for pos_x, cell in enumerate(row):
+                if cell == Cell.STARTING_POINT:
+                    starting_point = (pos_x, len(maze))
             maze.append(row)
-    return maze, starting_point, end_point
+    return maze, starting_point
 
 
-def next_move(
-    maze: List[List[str]], current_position: Tuple[int, int], heading: int
-) -> Tuple[Tuple[int, int], int]:
-    step = ((0, 1), (1, 0), (0, -1), (-1, 0))
-    for direction in (RIGHT, FORWARD, LEFT, BACKWARD):
-        new_heading = (heading + direction) % len(step)
+def get_cell(maze: List[List[str]], position: Tuple[int, int]) -> Cell:
+    return Cell(maze[position[1]][position[0]])
+
+
+def set_cell(maze: List[List[str]], position: Tuple[int, int], cell_value: Cell):
+    maze[position[1]][position[0]] = cell_value.value
+
+
+def find_path(
+    maze: List[List[str]],
+    current_position: Tuple[int, int],
+    heading: int,
+    path: List[Tuple[int, int]],
+) -> Optional[List[Tuple[int, int]]]:
+    path.append(current_position)
+    set_cell(maze, current_position, Cell.VISITED)
+    for move in (Move.RIGHT, Move.FORWARD, Move.LEFT, Move.BACKWARD):
+        new_heading = Direction((heading + move) % len(STEP))
         new_position = (
-            current_position[0] + step[new_heading][0],
-            current_position[1] + step[new_heading][1],
+            current_position[0] + STEP[new_heading][0],
+            current_position[1] + STEP[new_heading][1],
         )
-        cell_value = maze[new_position[1]][new_position[0]]
-        if cell_value != WALL and cell_value != VISITED:
-            return new_position, new_heading
-
-
-def solve(
-    maze: List[List[str]], current_position: Tuple[int, int], end_point: Tuple[int, int]
-) -> List[Tuple[int, int]]:
-    heading = NORTH
-    moves = [current_position]
-    while current_position != end_point:
-        maze[current_position[1]][current_position[0]] = VISITED
-        current_position, heading = next_move(maze, current_position, heading)
-        moves.append(current_position)
-    return moves
+        cell = get_cell(maze, new_position)
+        if cell == Cell.EXIT_POINT:
+            path.append(new_position)
+            return path
+        elif cell == Cell.EMPTY:
+            if path_to_exit := find_path(maze, new_position, new_heading, list(path)):
+                return path_to_exit
+    return None
 
 
 filename = sys.argv[1]
-print(solve(*load_from_file(filename)))
+print(find_path(*load_from_file(filename), Direction.NORTH, []))
